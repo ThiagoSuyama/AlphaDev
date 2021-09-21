3b09c6a47b69d189b1e4b620dc88de474780c0f7
yurigregorio <yuri.gregorio1@hotmail.com>
2021-09-20 21:57:51 -0300

    Initial commit for project

---
+++ src/main/java/br/com/istorage/istorage/IStorageApplication.java
@@ -0,0 +1,13 @@
+package br.com.istorage.istorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/istorage/istorage/IStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.istorage.istorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
